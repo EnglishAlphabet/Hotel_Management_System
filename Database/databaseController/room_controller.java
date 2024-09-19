45329bea1067f77ee526c196c2b768a7d8d6bf61
EnglishAlphabet <ggs5times@gmail.com>
2024-09-19 11:35:51 +0630

    Update room_controller.java

--- Database/databaseController/room_controller.java
+++ Database/databaseController/room_controller.java
@@ -34,6 +34,31 @@ public class room_controller {
             return null;
         }
     }
+    
+    public static List<room> getRoomByFloor(String f){
+        List<room> all_rooms = new ArrayList<>();
+        String query = "SELECT * FROM room WHERE floor = ?";
+        try(Connection con = DBConnection.getConnection();
+            PreparedStatement psmt = con.prepareStatement(query);){
+
+            psmt.setString(1, f);
+            ResultSet rs = psmt.executeQuery();
+            while(rs.next()){
+                String room_no = rs.getString(1);
+                String room_type = rs.getString(2);
+                int floor = rs.getInt(3);
+                String room_status = rs.getString(4);
+
+                room r = new room(room_no,new room_type(room_type,null), floor, new room_status(room_status,null));
+                all_rooms.add(r);
+            }
+
+            return all_rooms;
+        }catch(SQLException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(getAllRooms());
