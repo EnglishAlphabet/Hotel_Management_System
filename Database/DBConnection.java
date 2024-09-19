a364b1b92b8efddcfcb713a7076d774c0ab05fb0
EnglishAlphabet <ggs5times@gmail.com>
2024-09-19 10:30:36 +0630

    Update DBConnection.java

--- Database/DBConnection.java
+++ Database/DBConnection.java
@@ -1,6 +1,6 @@
 public class DBConnection {
     public static Connection getConnection() throws SQLException {
-        String url = "jdbc:mysql://localhost/";
+        String url = "jdbc:mysql://localhost/"; //name of database
         String username = "root";
         String password = "";
 
