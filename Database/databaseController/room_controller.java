1c307675c7eb25be48d038f9ad46a4a3c1022e62
EnglishAlphabet <ggs5times@gmail.com>
2024-09-19 10:06:57 +0630

    Create room_controller.java

---
+++ Database/databaseController/room_controller.java
@@ -0,0 +1,41 @@
+package beta.database.controller;
+
+import beta.connection.DBConnection;
+import beta.models.room;
+import beta.models.room_status;
+import beta.models.room_type;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
+
+public class room_controller {
+
+    public static List<room> getAllRooms(){
+        List<room> all_rooms = new ArrayList<>();
+        String query = "SELECT * FROM room";
+        try(Connection con = DBConnection.getConnection();
+        PreparedStatement psmt = con.prepareStatement(query);){
+
+            ResultSet rs = psmt.executeQuery();
+            while(rs.next()){
+                String room_no = rs.getString(1);
+                String room_type = rs.getString(2);
+                int floor = rs.getInt(3);
+                String room_status = rs.getString(4);
+
+                room r = new room(room_no,new room_type(room_type,null), floor, new room_status(room_status,null));
+                all_rooms.add(r);
+            }
+
+            return all_rooms;
+        }catch(SQLException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getAllRooms());
+    }
+}
