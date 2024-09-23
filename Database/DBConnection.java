cf9b5f81d4c534ba627a258b5fd641f318654d95
EnglishAlphabet <ggs5times@gmail.com>
2024-09-23 09:39:32 +0630

    Delete Database/DBConnection.java

--- Database/DBConnection.java
+++
@@ -1,9 +0,0 @@
-public class DBConnection {
-    public static Connection getConnection() throws SQLException {
-        String url = "jdbc:mysql://localhost/"; //name of database
-        String username = "root";
-        String password = "";
-
-        return DriverManager.getConnection(url, username, password);
-    }
-}
