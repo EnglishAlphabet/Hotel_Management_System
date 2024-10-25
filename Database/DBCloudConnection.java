17e0b1db2d9711336e0d338f47abce0b8d233b41
EnglishAlphabet <ggs5times@gmail.com>
2024-10-25 10:53:35 +0630

    Update DBCloudConnection.java

--- Database/DBCloudConnection.java
+++ Database/DBCloudConnection.java
@@ -1,8 +1,8 @@
 public class DBConnection {
     public static Connection getConnection() throws SQLException {
-        String url = "jdbc:mysql://hotel-oversunset-sunset-hotel.c.aivencloud.com:24376/sunset_hotel_db";
-        String username = "avnadmin";
-        String password = "AVNS_RaPaAPRKW7lPQKNNO3x";
+        String url = "";
+        String username = "";
+        String password = "";
 
         return DriverManager.getConnection(url, username, password);
 
